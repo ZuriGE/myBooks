dbfeb3bc52f9052210b3177a46beafff52d6a4a0
ZuriGE <zurigales@gmail.com>
2023-11-13 22:42:23 +0100

    formularios model driven

--- src/app/component/form-register/form-register.component.ts
+++ src/app/component/form-register/form-register.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl , ValidationErrors, ValidatorFn} from '@angular/forms';
 
 @Component({
   selector: 'app-form-register',
   templateUrl: './form-register.component.html',
   styleUrls: ['./form-register.component.css']
 })
-export class FormRegisterComponent {
+export class FormRegisterComponent implements OnInit {
 
+  public myForm: FormGroup;
+
+  constructor(private formBuilder:FormBuilder){
+    this.buildForm();
+  }
+
+  public register(){
+    const user = this.myForm.value;
+}
+
+  private buildForm(){
+    const minPassLength = 8;
+
+    this.myForm = this.formBuilder.group({
+      name: [, Validators.required],
+      surname: [, Validators.required],
+      email: [, [Validators.required, Validators.email]],
+      password: [,[ Validators.required, Validators.minLength(minPassLength), this.safePassword]],
+      passwordRepeat: [,[Validators.required, this.checkPasswords]],
+      photo: [, Validators.required],
+      
+
+    })
+  }
+
+ private safePassword(control: AbstractControl){
+  let resultado = {passwordUnsafe: true}
+ 
+  const hasUpperCase = /[A-Z]+/.test(control.value);
+  const hasLowerCase = /[a-z]+/.test(control.value);
+  const hasNumeric = /[0-9]+/.test(control.value);
+
+  const passwordValid = hasUpperCase && hasLowerCase && hasNumeric
+
+  if (passwordValid){
+    resultado=null
+  }else if(!control.value){
+    resultado=null
+  }
+  return resultado
+}
+
+
+private checkPasswords(control: AbstractControl){
+  let resultado = {matchPassword: true}
+  if(control.parent?.value.password == control.value){
+    resultado = null;
+  }
+  return resultado
+}
+
+showPassword: boolean = false;
+
+showPw() {
+  this.showPassword = !this.showPassword;
+}
+
+showPasswordRep: boolean = false;
+
+showPwRepeat() {
+  this.showPasswordRep = !this.showPasswordRep;
+  console.log(this.showPasswordRep)
+}
+
+ngOnInit():void{}
 }
